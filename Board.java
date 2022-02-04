4a40f9e2cc00f592f3846d5ef7c5b1f769a52055
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-02-04 13:17:47 -0800

    Create Board.java

---
+++ Board.java
@@ -0,0 +1,22 @@
+public class Board {
+    
+    //attirbutes 
+    private static int pieces; 
+    
+     /* your code here - constructor(s) */ 
+     public static void populate()
+     {
+
+     }
+
+   //accessors
+     public static int getNumPieces()
+     {
+
+     }
+    public static void removePieces(int numRemoved)
+    {
+        
+    }
+    
+}// end of class
