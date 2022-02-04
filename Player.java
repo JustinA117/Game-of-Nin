74641f531aebc7756d9d44a741adc86013f7b927
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-02-04 13:18:02 -0800

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,28 @@
+public class Player {
+
+    //attributes
+    private String name;
+    
+    private int score;
+
+    private int turn;
+    
+    public int takeTurn()
+    {
+        return turn;
+    }
+ // accessor
+    public String getName()
+    {
+        return name; 
+    } 
+
+    public int getScore()
+    {
+        return score;
+    }
+    public void incrScore()
+    {
+        return score;
+    }
+} // end of whole class
