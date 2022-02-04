635a1efe5516983d05e4a6a172ab66bce146dc20
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-02-04 13:17:12 -0800

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,23 @@
+public class Game
+{
+    private Player player1;
+    private Player player2;
+    // constructors
+    public void play()
+    {
+
+    }
+private boolean playAgain()
+{
+    
+}
+
+
+
+
+
+
+
+
+
+} // end of whole class
