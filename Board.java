f6d2f9db941e440eeffa984fa6878061ad254c4c
JustinA117 <94567481+JustinA117@users.noreply.github.com>
2022-02-07 12:32:34 -0800

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,18 +1,19 @@
 public class Board {
-    
+    int numPieces = 0;
+
     //attirbutes 
     private static int pieces; 
     
      /* your code here - constructor(s) */ 
      public static void populate()
      {
-
+      
      }
 
    //accessors
      public static int getNumPieces()
      {
-
+      Math.random()
      }
     public static void removePieces(int numRemoved)
     {
